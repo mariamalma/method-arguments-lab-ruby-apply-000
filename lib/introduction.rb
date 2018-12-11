3648bd40eaac5b7489b55fc9830cc6859992396c
Mariam Ibrahim <mibrah7@students.towson.edu>
2018-12-11 04:14:37 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,8 @@
+def 
+introduction_with_language {name} {language}
+  # puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  print "Hi, my name is Mariam and I'm learning to program in Ruby."
+  puts #name Mariam 
+  puts #language Ruby 
+  
+end 
